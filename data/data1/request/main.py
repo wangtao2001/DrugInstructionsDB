5351798edf5a4d91b612ce14d49f03f08b092c60
wangtao2001 <wangtao.cpu@gmail.com>
2021-07-31 17:07:35 +0800

    finish get new data

--- data/data1/request/main.py
+++ data/data1/request/main.py
@@ -3,24 +3,24 @@ import requests
 import pandas as pd
 import re
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Connection': 'keep-alive'}
+
 
 class GetData:
     def __init__(self, id_number):
         root_url = "http://data.baidu1y.com/data/yytextdetail.aspx"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'Connection': 'keep-alive'
-        }
         self.id = str(id_number)
         params = {'id': self.id}
         r = requests.get(root_url, headers=headers, params=params, timeout=15)
         soup = bs4.BeautifulSoup(r.text, 'html.parser')
         self.drug_name = soup.find('div', attrs={'class': 'zhanhtit2'}).string.strip()
+        print(self.drug_name)
         self.div_list = soup.find_all('div', attrs={'class': 'padd6'})
 
     def get_BasicInfo(self): # 基本信息
-        """id approvalNum drugsType prescriptions validityPeriod"""
+        """approvalNum drugsType prescriptions validityPeriod specification enterprise"""
         basic_info = [self.id]
         try:
             approvalNum = self.div_list[5].find('div', attrs={'class': 'zh2'}).string
@@ -46,10 +46,23 @@ class GetData:
             validityPeriod = 'NULL'
         finally:
             basic_info.append(validityPeriod)
+        try:
+            specification = self.div_list[11].find('div', attrs={'class': 'zh2'}).string
+        except TypeError:
+            specification = 'NULL'
+        finally:
+            basic_info.append(specification)
+        try:
+            enterprise = self.div_list[4].find('div', attrs={'class': 'zh4'}).string
+        except TypeError:
+            enterprise = 'NULL'
+        finally:
+            basic_info.append(enterprise)
 
         return basic_info
 
     def get_DrugName(self):  # 药物名称
+        """id productName commonName pinyinName"""
         drug_name = [self.id]
         try:
             productName = self.div_list[1].find('div', attrs={'class': 'zh2'}).string
@@ -142,7 +155,7 @@ class GetData:
 
         return main_compounds
 
-    def get_PharmacologyToxicology(self):  # 主要成分
+    def get_PharmacologyToxicology(self):  # 药理学毒理学
         pharmacology_toxicology = [self.id]
         try:
             item = self.div_list[20].find('div', attrs={'class': 'zh2'}).string
@@ -152,6 +165,17 @@ class GetData:
             pharmacology_toxicology.append(item)
 
         return pharmacology_toxicology
+
+    def get_Pharmacokinetics(self):  # 药代动力学
+        pharmacokinetics = [self.id]
+        try:
+            item = self.div_list[21].find('div', attrs={'class': 'zh4'}).string
+        except TypeError:
+            item = 'NULL'
+        finally:
+            pharmacokinetics.append(item)
+
+        return pharmacokinetics
 
     def get_Precautions(self):  # 注意事项
         """多列表嵌套"""
@@ -166,6 +190,19 @@ class GetData:
 
         return [[self.id, item] for item in re.split('<br/>|\n', precautions[0])]
 
+    def get_Taboo(self):  # 禁忌
+        """多列表嵌套"""
+        taboo = []
+        try:
+            item = str(self.div_list[14].find('div', attrs={'class': 'zh2'})).lstrip('<div class="zh2">').rstrip(
+                '</div>')
+        except TypeError:
+            item = 'NULL'
+        finally:
+            taboo.append(item)
+
+        return [[self.id, item] for item in re.split('<br/>|\n', taboo[0])]
+
     def get_Storage(self):  # 贮藏
         storage = [self.id]
         try:
@@ -177,7 +214,62 @@ class GetData:
 
         return storage
 
+    def get_SpecialCrowd(self):  # 特殊人群用药
+        "pregnant child elderly"
+        special_crowd = [self.id]
+        try:
+            pregnant = self.div_list[16].find('div', attrs={'class': 'zh2'}).string
+        except TypeError:
+            pregnant = 'NULL'
+        finally:
+            special_crowd.append(pregnant)
+        try:
+            child = self.div_list[17].find('div', attrs={'class': 'zh4'}).string
+        except TypeError:
+            child = 'NULL'
+        finally:
+            special_crowd.append(child)
+        try:
+            elderly = self.div_list[18].find('div', attrs={'class': 'zh2'}).string
+        except TypeError:
+            elderly = 'NULL'
+        finally:
+            special_crowd.append(elderly)
+
+        return special_crowd
+
 
 if __name__ == "__main__":
-    g = GetData(2020618155041112)
-    print(g.get_Storage())
+    # BasicInfo = open('../BasicInfo.xlsx','a')
+    # DrugName = open('../BasicInfo.xlsx','a')
+    # Character = open('../BasicInfo.xlsx', 'a')
+    # Dosage = open('../BasicInfo.xlsx', 'a')
+    # AdverseReaction = open('../BasicInfo.xlsx', 'a')
+    # DrugInteraction = open('../BasicInfo.xlsx', 'a')
+    # Indication = open('../BasicInfo.xlsx', 'a')
+    # MainCompounds = open('../BasicInfo.xlsx', 'a')
+    # PharmacologyToxicology = open('../BasicInfo.xlsx', 'a')
+    # Pharmacokinetics = open('../BasicInfo.xlsx', 'a')
+    # Precautions = open('../BasicInfo.xlsx', 'a')
+    # Taboo = open('../BasicInfo.xlsx', 'a')
+    # Storage = open('../BasicInfo.xlsx', 'a')
+    # SpecialCrowd = open('../BasicInfo.xlsx', 'a')
+
+    url = 'http://data.baidu1y.com/data/yytext.aspx'
+    data = pd.read_excel('../../../resources/国家药品编码本位码信息（国产药品）.xlsx', engine='openpyxl', header=2)
+    for i in range(0,149344):
+        ApproveCode = data.iloc[i]['批准文号']
+        print(i)
+        r = requests.get(url, headers=headers, params={'ApproveCode': ApproveCode}, timeout=15)
+        try:
+            number = bs4.BeautifulSoup(r.text, 'html.parser').find_all('div', attrs={'class': 'yyaodata29'})[1].find('a').get('href').split('?')[1][3:]
+        except IndexError:
+            continue
+        if number != '':
+            g = GetData(number)
+
+
+
+
+
+
